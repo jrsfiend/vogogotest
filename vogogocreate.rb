18b6c83765e095f18a4255b44ea5cda91388e4a9
Jarett Dunn <jarettrsdunn@gmail.com>
2014-10-14 18:24:04 -0300

    removed redundancy in vogogocreate.rb

--- vogogocreate.rb
+++ vogogocreate.rb
@@ -1,15 +1,82 @@
 class Machine
+	def write(d, which, filename)
+		line1 = []
+		line2 = []
+		line3 = []
+		d.each_with_index do |val, index| 
+			if val == 0 
+				line1[index] = " _ "
+				line2[index] = "| |"
+				line3[index] = "|_|"
+			elsif val == 1
+				line1[index] = "   "
+				line2[index] = "  |"
+				line3[index] = "  |"
+			elsif val == 2
+				line1[index] = " _ "
+				line2[index] = " _|"
+				line3[index] = "|_ "
+			elsif val == 3
+				line1[index] = " _ "
+				line2[index] = " _|"
+				line3[index] = " _|"
+			elsif val == 4
+				line1[index] = "   "
+				line2[index] = "|_|"
+				line3[index] = "  |"
+			elsif val == 5
+				line1[index] = " _ "
+				line2[index] = "|_ "
+				line3[index] = " _|"
+			elsif val == 6
+				line1[index] = " _ "
+				line2[index] = "|_ "
+				line3[index] = "|_|"
+			elsif val == 7
+				line1[index] = " _ "
+				line2[index] = "  |"
+				line3[index] = "  |"
+			elsif val == 8
+				line1[index] = " _ "
+				line2[index] = "|_|"
+				line3[index] = "|_|"
+			elsif val == 9
+				line1[index] = " _ "
+				line2[index] = "|_|"
+				line3[index] = " _|"
+			end
+		end
+		if @valids > @illegals
+			random = rand(9)
+			line1[random] = "|||"
+			@illegals = @illegals + 1
+		else
+			if which == "valids" 
+				@valids  = @valids + 1
+			elsif which == "errors"
+				@errors = @errors + 1
+			end
+		end
+		File.open(filename, "a+") do |f|
+			line1.each { |element| f.write(element) }
+			f.puts("\n")
+			line2.each { |element| f.write(element) }
+			f.puts("\n")
+			line3.each { |element| f.write(element) }
+			f.puts("\n")
+			f.puts("\n")
+		end
+	end
 	def initialize(filename)
-		@filename = filename
-		File.open(@filename, "w+") do |f|
+		@errors = 0
+		@valids = 0
+		@illegals = 0
+		File.open(filename, "w+") do |f|
 
 		end
-		valids = 0
-		errors = 0 
 		done = false
-		illegals = 0
 		while !done do
-			if (valids + errors + illegals) >= 125
+			if (@valids + @errors + @illegals) >= 125
 				done = true
 			end
 
@@ -24,126 +91,18 @@ class Machine
 			d[1] = rand(9)
 			d[0] = rand(9)
 			if ((1*d[8])+(2*d[7])+(3*d[6])+(4*d[5])+(5*d[4])+(6*d[3])+(7*d[2])+(8*d[1])+(9*d[0])) % 11 == 0
-				line1 = []
-				line2 = []
-				line3 = []
-				if valids <= 41
-					
-					d.each_with_index do |val, index| 
-						if val == 0 
-							line1[index] = " _ "
-							line2[index] = "| |"
-							line3[index] = "|_|"
-						elsif val == 1
-							line1[index] = "   "
-							line2[index] = "  |"
-							line3[index] = "  |"
-						elsif val == 2
-							line1[index] = " _ "
-							line2[index] = " _|"
-							line3[index] = "|_ "
-						elsif val == 3
-							line1[index] = " _ "
-							line2[index] = " _|"
-							line3[index] = " _|"
-						elsif val == 4
-							line1[index] = "   "
-							line2[index] = "|_|"
-							line3[index] = "  |"
-						elsif val == 5
-							line1[index] = " _ "
-							line2[index] = "|_ "
-							line3[index] = " _|"
-						elsif val == 6
-							line1[index] = " _ "
-							line2[index] = "|_ "
-							line3[index] = "|_|"
-						elsif val == 7
-							line1[index] = " _ "
-							line2[index] = "  |"
-							line3[index] = "  |"
-						elsif val == 8
-							line1[index] = " _ "
-							line2[index] = "|_|"
-							line3[index] = "|_|"
-						elsif val == 9
-							line1[index] = " _ "
-							line2[index] = "|_|"
-							line3[index] = " _|"
-						end
-					end
-					if valids > illegals
-						random = rand(9)
-						line1[random] = "|||"
-						illegals = illegals + 1
-					else
-						valids  = valids + 1
-					end
-					write(line1, line2, line3)
+				
+				if @valids <= 41
+					write(d, "valids", filename)
 				end
 			else
 				line1 = []
 				line2 = []
 				line3 = []	
-				if errors <= 41 and valids > errors
-					d.reverse.each_with_index do |val, index| 
-						if val == 0 
-							line1[index] = " _ "
-							line2[index] = "| |"
-							line3[index] = "|_|"
-						elsif val == 1
-							line1[index] = "   "
-							line2[index] = "  |"
-							line3[index] = "  |"
-						elsif val == 2
-							line1[index] = " _ "
-							line2[index] = " _|"
-							line3[index] = "|_ "
-						elsif val == 3
-							line1[index] = " _ "
-							line2[index] = " _|"
-							line3[index] = " _|"
-						elsif val == 4
-							line1[index] = "   "
-							line2[index] = "|_|"
-							line3[index] = "  |"
-						elsif val == 5
-							line1[index] = " _ "
-							line2[index] = "|_ "
-							line3[index] = " _|"
-						elsif val == 6
-							line1[index] = " _ "
-							line2[index] = "|_ "
-							line3[index] = "|_|"
-						elsif val == 7
-							line1[index] = " _ "
-							line2[index] = "  |"
-							line3[index] = "  |"
-						elsif val == 8
-							line1[index] = " _ "
-							line2[index] = "|_|"
-							line3[index] = "|_|"
-						elsif val == 9
-							line1[index] = " _ "
-							line2[index] = "|_|"
-							line3[index] = " _|"
-						end
-					end
-				write(line1, line2, line3)
-				errors = errors + 1
+				if @errors <= 41 and @valids > @errors
+					write(d, "errors", filename)
 				end
 			end
-		end
-	end
-	def write(line1, line2, line3)
-		File.open(@filename, "a+") do |f|
-			line1.each { |element| f.write(element) }
-			f.puts("\n")
-			line2.each { |element| f.write(element) }
-			f.puts("\n")
-			line3.each { |element| f.write(element) }
-			f.puts("\n")
-			f.puts("\n")
 		end
 	end
 
